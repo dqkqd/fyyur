bb5902dc0452c6822e2080525514f546ace78fdc
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-07-29 23:33:16 +0900

    Refactor to remove duplicated codes.

--- tests/test_show.py
+++ tests/test_show.py
@@ -1,9 +1,10 @@
 import pytest
+from flask import Response
 
 from fyyur.model import Show, db
 from fyyur.routes.show import get_shows
 from fyyur.schema.show import ShowResponse
-from tests.mock import mock_artist, mock_show, mock_venue
+from tests.mock import date_future, mock_artist, mock_show, mock_venue
 
 
 def test_get_shows_status_200(client):
@@ -11,6 +12,11 @@ def test_get_shows_status_200(client):
     assert response.status_code == 200
     assert b"Venue1" in response.data
     assert b"Artist1" in response.data
+
+
+def add_show(client, venue_id: int, artist_id: int, day_offset: int) -> Response:
+    show = mock_show(venue_id=venue_id, artist_id=artist_id, day_offset=day_offset)
+    return client.post("/shows/create", data=show.model_dump())
 
 
 def test_create_show_successful(app, client):
@@ -25,39 +31,34 @@ def test_create_show_successful(app, client):
     with app.app_context():
         assert not Show.query.filter_by(venue_id=100, artist_id=200).all()
 
-    show_data = mock_show(venue_id=100, artist_id=200, day_offset=100).model_dump()
-
-    response = client.post(
-        "/shows/create",
-        data=show_data,
-    )
-
     # inserted into database
+    response = add_show(client=client, venue_id=100, artist_id=200, day_offset=100)
     assert response.status_code == 200
+
     with app.app_context():
         shows = Show.query.filter_by(venue_id=100, artist_id=200).all()
         assert len(shows) == 1
 
 
 def test_get_shows(app, client):
-    show1 = mock_show(venue_id=1, artist_id=1, day_offset=100)
-    show2 = mock_show(venue_id=1, artist_id=2, day_offset=200)
-
-    for show in [show1, show2]:
-        client.post(
-            "/shows/create",
-            data=show.model_dump(),
-        )
+    response = add_show(client=client, venue_id=1, artist_id=1, day_offset=100)
+    assert response.status_code == 200
+    response = add_show(client=client, venue_id=1, artist_id=2, day_offset=200)
+    assert response.status_code == 200
 
     expected_shows = [
         ShowResponse(
-            **show1.model_dump(),
+            venue_id=1,
+            artist_id=1,
+            start_time=date_future(100),
             venue_name="Venue1",
             artist_name="Artist1",
             artist_image_link="https://example1.com/",
         ),
         ShowResponse(
-            **show2.model_dump(),
+            venue_id=1,
+            artist_id=2,
+            start_time=date_future(200),
             venue_name="Venue1",
             artist_name="Artist2",
             artist_image_link="https://example2.com/",
@@ -74,10 +75,8 @@ def test_get_shows(app, client):
 def test_create_show_venue_or_artist_doesnt_exist(
     app, client, venue_id: int, artist_id: int
 ):
-    show = mock_show(venue_id=venue_id, artist_id=artist_id, day_offset=100)
-    response = client.post(
-        "/shows/create",
-        data=show.model_dump(),
+    response = add_show(
+        client=client, venue_id=venue_id, artist_id=artist_id, day_offset=100
     )
 
     # redirecting to /shows/create again
@@ -89,64 +88,32 @@ def test_create_show_venue_or_artist_doesnt_exist(
 
 
 def test_create_show_duplicated(client):
-    show = mock_show(venue_id=1, artist_id=1, day_offset=100)
-
-    response = client.post(
-        "/shows/create",
-        data=show.model_dump(),
-    )
+    response = add_show(client=client, venue_id=1, artist_id=1, day_offset=100)
     assert response.status_code == 200
 
-    response = client.post(
-        "/shows/create",
-        data=show.model_dump(),
-    )
+    response = add_show(client=client, venue_id=1, artist_id=1, day_offset=100)
     assert response.status_code == 302
 
 
 def test_create_show_same_date_same_venue(client):
-    show1 = mock_show(venue_id=1, artist_id=1, day_offset=100)
-    response = client.post(
-        "/shows/create",
-        data=show1.model_dump(),
-    )
+    response = add_show(client=client, venue_id=1, artist_id=1, day_offset=100)
     assert response.status_code == 200
 
-    show2 = mock_show(venue_id=1, artist_id=2, day_offset=100)
-    response = client.post(
-        "/shows/create",
-        data=show2.model_dump(),
-    )
+    response = add_show(client=client, venue_id=1, artist_id=2, day_offset=100)
     assert response.status_code == 302
 
 
 def test_create_show_same_date_same_artist(client):
-    show1 = mock_show(venue_id=1, artist_id=1, day_offset=100)
-    response = client.post(
-        "/shows/create",
-        data=show1.model_dump(),
-    )
+    response = add_show(client=client, venue_id=1, artist_id=1, day_offset=100)
     assert response.status_code == 200
 
-    show2 = mock_show(venue_id=2, artist_id=1, day_offset=100)
-    response = client.post(
-        "/shows/create",
-        data=show2.model_dump(),
-    )
+    response = add_show(client=client, venue_id=2, artist_id=1, day_offset=100)
     assert response.status_code == 302
 
 
 def test_create_show_same_date_different_venue_and_artist(client):
-    show1 = mock_show(venue_id=1, artist_id=1, day_offset=100)
-    response = client.post(
-        "/shows/create",
-        data=show1.model_dump(),
-    )
+    response = add_show(client=client, venue_id=1, artist_id=1, day_offset=100)
     assert response.status_code == 200
 
-    show2 = mock_show(venue_id=2, artist_id=2, day_offset=100)
-    response = client.post(
-        "/shows/create",
-        data=show2.model_dump(),
-    )
+    response = add_show(client=client, venue_id=2, artist_id=2, day_offset=100)
     assert response.status_code == 200
