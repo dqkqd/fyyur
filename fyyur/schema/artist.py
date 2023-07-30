4b750f67c2b152361535c77b0b01254b4aabfa9f
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-07-30 15:02:00 +0900

    Separate genres inside and outside db.

--- fyyur/schema/artist.py
+++ fyyur/schema/artist.py
@@ -5,7 +5,7 @@ from pydantic import HttpUrl, field_serializer
 
 from fyyur.model import Artist, Show
 from fyyur.schema.base import BaseSchema, State
-from fyyur.schema.genre import GenreInDb
+from fyyur.schema.genre import GenreEnum, GenreInDb
 from fyyur.schema.show import ShowInArtistInfo, ShowInDb
 
 
@@ -28,7 +28,7 @@ class ArtistSearchResponse(ArtistBase):
         return artist_search_response
 
 
-class ArtistBasicInfo(ArtistBase):
+class ArtistBasicInfoBase(ArtistBase):
     city: str | None = None
     state: State | None = None
     phone: str | None = None
@@ -39,8 +39,6 @@ class ArtistBasicInfo(ArtistBase):
 
     seeking_venue: bool = False
     seeking_description: str | None = None
-
-    genres: list[GenreInDb] = []
 
     @field_serializer("image_link", "facebook_link", "website_link", return_type=str)
     def serialize_url(self, url: HttpUrl) -> str:
@@ -53,12 +51,23 @@ class ArtistBasicInfo(ArtistBase):
         return state.value
 
 
-class ArtistInDb(ArtistBasicInfo):
+class ArtistBasicInfo(ArtistBasicInfoBase):
+    genres: list[GenreEnum] = []
+
+
+class ArtistInDb(ArtistBasicInfoBase):
     id: int
     shows: list[ShowInDb] = []
+    genres: list[GenreInDb] = []
+
+    def to_artist_basic_info(self) -> ArtistBasicInfo:
+        return ArtistBasicInfo(
+            **self.model_dump(exclude=["id", "shows", "genres"]),
+            genres=[genre.name.value for genre in self.genres]
+        )
 
 
-class ArtistInfoResponse(ArtistBasicInfo):
+class ArtistInfoResponse(ArtistBasicInfoBase):
     past_shows: list[ShowInArtistInfo]
     upcoming_shows: list[ShowInArtistInfo]
     past_shows_count: int
@@ -66,7 +75,7 @@ class ArtistInfoResponse(ArtistBasicInfo):
 
     @classmethod
     def from_artist(cls, artist: Artist) -> Self:
-        artist_in_db_base = ArtistBasicInfo.model_validate(artist)
+        artist_info = ArtistInDb.model_validate(artist).to_artist_basic_info()
 
         def is_past(show: Show) -> bool:
             return show.start_time < datetime.now()
@@ -80,7 +89,7 @@ class ArtistInfoResponse(ArtistBasicInfo):
         )
 
         return cls(
-            **artist_in_db_base.model_dump(),
+            **artist_info.model_dump(),
             past_shows=past_shows,
             upcoming_shows=upcoming_shows,
             past_shows_count=len(past_shows),
