5529f5a9745e6c1edb404ba6c10d99ade6be0f9e
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-07-31 01:22:08 +0900

    Use json data for testing.

--- tests/test_artist.py
+++ tests/test_artist.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import Any
 
 import pytest
@@ -9,7 +10,7 @@ from fyyur.routes.artist import find_artists, get_artist_info, get_artists
 from fyyur.schema.base import SearchSchema
 from fyyur.schema.genre import GenreEnum
 from tests.mock import mock_artist, mock_genre, mock_show
-from tests.utils import date_past
+from tests.utils import date_future, date_past, same_time
 
 
 def test_get_artists(app: Flask) -> None:
@@ -86,41 +87,152 @@ def test_find_artists_with_past_shows(app: Flask) -> None:
 
 
 @pytest.mark.parametrize(
-    "artist_id, genres",
+    "artist_id, expected_artist_data",
     [
-        (1, [GenreEnum.Blues, GenreEnum.HipHop, GenreEnum.Jazz]),
-        (2, [GenreEnum.Jazz, GenreEnum.RockNRoll, GenreEnum.Pop]),
-        (3, [GenreEnum.Pop]),
-        (4, []),
+        (
+            1,
+            {
+                "id": 1,
+                "name": "Artist1",
+                "genres": ["Blues", "Hip-Hop", "Jazz"],
+                "city": "San Francisco",
+                "state": "CA",
+                "phone": "326-123-5000",
+                "image_link": "https://images.artist1.com/",
+                "website_link": "https://artist1.com/",
+                "facebook_link": "https://www.facebook.com/artist1/",
+                "seeking_venue": True,
+                "seeking_description": "Artist1 Looking for shows",
+                "past_shows": [],
+                "upcoming_shows": [
+                    {
+                        "venue_id": 1,
+                        "venue_name": "Venue1",
+                        "venue_image_link": "https://images.venue1.com/",
+                        "start_time": date_future(1),
+                    },
+                    {
+                        "venue_id": 1,
+                        "venue_name": "Venue1",
+                        "venue_image_link": "https://images.venue1.com/",
+                        "start_time": date_future(2),
+                    },
+                ],
+                "past_shows_count": 0,
+                "upcoming_shows_count": 2,
+            },
+        ),
+        (
+            2,
+            {
+                "id": 2,
+                "name": "Artist2",
+                "genres": ["Jazz", "Rock n Roll", "Pop"],
+                "city": "San Francisco",
+                "state": "CA",
+                "phone": "326-123-5000",
+                "image_link": "https://images.artist2.com/",
+                "website_link": "https://artist2.com/",
+                "facebook_link": "https://www.facebook.com/artist2/",
+                "seeking_venue": True,
+                "seeking_description": "Artist2 Looking for shows",
+                "past_shows": [],
+                "upcoming_shows": [
+                    {
+                        "venue_id": 2,
+                        "venue_name": "Venue2",
+                        "venue_image_link": "https://images.venue2.com/",
+                        "start_time": date_future(3),
+                    }
+                ],
+                "past_shows_count": 0,
+                "upcoming_shows_count": 1,
+            },
+        ),
+        (
+            3,
+            {
+                "id": 3,
+                "name": "Artist3",
+                "genres": ["Pop"],
+                "city": "San Francisco",
+                "state": "CA",
+                "phone": "326-123-5000",
+                "image_link": "https://images.artist3.com/",
+                "website_link": "https://artist3.com/",
+                "facebook_link": "https://www.facebook.com/artist3/",
+                "seeking_venue": True,
+                "seeking_description": "Artist3 Looking for shows",
+                "past_shows": [],
+                "upcoming_shows": [
+                    {
+                        "venue_id": 1,
+                        "venue_name": "Venue1",
+                        "venue_image_link": "https://images.venue1.com/",
+                        "start_time": date_future(4),
+                    }
+                ],
+                "past_shows_count": 0,
+                "upcoming_shows_count": 1,
+            },
+        ),
+        (
+            4,
+            {
+                "id": 4,
+                "name": "Artist4",
+                "genres": [],
+                "city": "San Francisco",
+                "state": "CA",
+                "phone": "326-123-5000",
+                "image_link": "https://images.artist4.com/",
+                "website_link": "https://artist4.com/",
+                "facebook_link": "https://www.facebook.com/artist4/",
+                "seeking_venue": True,
+                "seeking_description": "Artist4 Looking for shows",
+                "past_shows": [
+                    {
+                        "venue_id": 2,
+                        "venue_name": "Venue2",
+                        "venue_image_link": "https://images.venue2.com/",
+                        "start_time": date_past(4),
+                    }
+                ],
+                "upcoming_shows": [],
+                "past_shows_count": 1,
+                "upcoming_shows_count": 0,
+            },
+        ),
     ],
 )
-def test_get_artist_info(app: Flask, artist_id: int, genres: list[GenreEnum]) -> None:
-    artist = mock_artist(id=artist_id, seeking_venue=True)
+def test_get_artist_info(
+    app: Flask, artist_id: int, expected_artist_data: dict[str, Any]
+) -> None:
     with app.app_context():
-        upcoming_shows: list[Show] = (
-            Show.query.filter_by(artist_id=artist_id)
-            .filter(Show.start_time >= date_future(days=0))
-            .all()
-        )
-        past_shows: list[Show] = (
-            Show.query.filter_by(artist_id=artist_id)
-            .filter(Show.start_time < date_future(days=0))
-            .all()
-        )
-        expected_artist_info_response = artist.to_orm(
-            Artist
-        ).artist_info_response.model_copy(
-            update=dict(
-                genres=genres,
-                past_shows=[show.show_in_artist_info for show in past_shows],
-                upcoming_shows=[show.show_in_artist_info for show in upcoming_shows],
-                past_shows_count=len(past_shows),
-                upcoming_shows_count=len(upcoming_shows),
-            ),
-        )
-
         artist_info_response = get_artist_info(artist_id=artist_id)
-        assert expected_artist_info_response == artist_info_response
+        assert artist_info_response is not None
+
+        modified_artist_data = expected_artist_data.copy()
+        artist_info_response_dumped = artist_info_response.model_dump()
+        for key in ["past_shows", "upcoming_shows"]:
+            shows = artist_info_response_dumped.get(key, [])
+            expected_shows = expected_artist_data.get(key, [])
+            assert len(shows) == len(expected_shows)
+
+            for i in range(len(shows)):
+                show_time = shows[i].get("start_time", None)
+                expected_show_time = expected_shows[i].get("start_time", None)
+
+                assert show_time is not None
+                assert expected_show_time is not None
+
+                assert isinstance(show_time, datetime)
+                assert isinstance(expected_show_time, datetime)
+                assert same_time(show_time, expected_show_time)
+
+                modified_artist_data[key][i]["start_time"] = show_time
+
+        assert modified_artist_data == artist_info_response.model_dump()
 
 
 def test_show_artist(client: FlaskClient) -> None:
