6719577ae734a351956f65473ab595f2a4f388b7
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-08-02 00:08:32 +0900

    Add more venue into test_get_venues.

--- tests/test_venue.py
+++ tests/test_venue.py
@@ -1,9 +1,18 @@
 from flask import Flask
 
+from fyyur.models import Venue, db
 from fyyur.routes.venue import get_venues
+from fyyur.schema.base import State
+from tests.mock import mock_venue
 
 
 def test_get_venues(app: Flask) -> None:
+    with app.app_context():
+        venue1 = mock_venue(id=10, city="test1", state=State.NY)
+        venue2 = mock_venue(id=11, city="test2", state=State.NY)
+        db.session.add_all([venue1.to_orm(Venue), venue2.to_orm(Venue)])
+        db.session.commit()
+
     expected_venues_data = [
         {
             "city": "San Francisco",
@@ -13,7 +22,21 @@ def test_get_venues(app: Flask) -> None:
                 {"name": "Venue2", "id": 2, "num_upcoming_shows": 1},
                 {"name": "Venue3", "id": 3, "num_upcoming_shows": 0},
             ],
-        }
+        },
+        {
+            "city": "test1",
+            "state": "NY",
+            "venues": [
+                {"name": "Venue10", "id": 10, "num_upcoming_shows": 0},
+            ],
+        },
+        {
+            "city": "test2",
+            "state": "NY",
+            "venues": [
+                {"name": "Venue11", "id": 11, "num_upcoming_shows": 0},
+            ],
+        },
     ]
 
     with app.app_context():
